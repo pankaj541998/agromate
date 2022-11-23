a4c04bd3933aab0f6da9cff2c339eb7d423df8b0
Pankaj Gupta <pankaj@wdimails.com>
2022-11-23 11:39:24 +0530

    merged code

--- lib/pages/Inventory_modes_of_application.dart
+++ lib/pages/Inventory_modes_of_application.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_agro_new/component/top_bar.dart';
 
 class ModesOfApplication extends StatefulWidget {
-  const ModesOfApplication({super.key});
+  const ModesOfApplication({Key? key}) : super(key: key);
 
   @override
   State<ModesOfApplication> createState() => _ModesOfApplicationState();
