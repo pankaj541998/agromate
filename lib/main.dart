9523670a7272021d5e57fe06f2a4957fdd3ffb2b
reethik <reethik1@gmail.com>
2022-12-08 20:39:43 +0530

    todays changes completed

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      initialRoute: '/',
+      initialRoute: '/classandtype',
       getPages: [
         GetPage(name: '/', page: () => const Login()),
         GetPage(name: '/register', page: () => const Register()),
